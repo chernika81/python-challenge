5a72e9d53d32ba79d53a12c3fc62c45ad141fd64
chernika81@yahoo.com <arminearutyunyan@Armines-MBP.attlocal.net>
2023-04-04 01:22:08 -0500

    completed python-challenge hw

---
+++ PyBank/mainpybank.py
@@ -0,0 +1,136 @@
+# Modules
+import os
+import csv
+
+# Define the path of the csv file
+pybank_csv = os.path.join("/Users/arminearutyunyan/Desktop/python-challenge1/python-challenge/PyBank/Resources/budget_data.csv")
+
+
+# Defining the variables
+total_months = 0
+profit_loss = 0
+net_total_profit_loss = 0
+month_before_profit_loss = 0
+change = 0
+greatest_increase = 0
+greatest_decrease = 0
+
+
+
+
+# Make lists to store column values
+date = []
+change_total = []
+greatest_increase_month = []
+greatest_decrease_month = []
+change =[]
+
+
+
+
+# Open the csv with the csv.reader
+with open(pybank_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+
+    # Header
+    csv_header = next(csvfile)
+
+    # Counting the total for date column
+    for row in csv_reader:
+        total_months += 1
+       
+
+        # Finding the total for profit/losses column
+        profit_loss = int(row[1])
+        net_total_profit_loss += profit_loss
+
+        
+        # opening a loop to find the change in profit and loss
+        
+        if (total_months ==1):
+            month_before_profit_loss = profit_loss
+            continue
+        
+        else:
+            change = profit_loss - month_before_profit_loss  
+            
+            date.append(row[0])
+            change_total.append(change)
+        
+            month_before_profit_loss = profit_loss
+            
+        
+        
+
+    # Finding the totals for the average calculation       
+        
+    sum_change_total = sum(change_total)
+    ave_change_total = round(sum_change_total/(total_months - 1), 2)
+
+
+# Finding the greatest increase in profits and greatest decrease in profits
+    for change in change_total:
+        if change > greatest_increase:
+            greatest_increase = change
+            greatest_increase_month = row[0]
+
+        if change < greatest_decrease:
+            greatest_decrease = change
+            greatest_decrease_month = row[0]
+        
+                                      
+
+
+# Terminal printing
+print("Financial Analysis")
+print("---------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total_profit_loss}")
+print(f"Average Change: ${ave_change_total}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
+
+# Text file printing. Defining a path to the output file
+output_file = os.path.join("/Users/arminearutyunyan/Desktop/PythonStuff/output/output_pybank.txt")
+with open(output_file, "w") as txt_file:
+    txt_file.write("Financial Analysis\n")
+    txt_file.write("--------------------------------\n")
+    txt_file.write(f"Total Months: {total_months}\n")
+    txt_file.write(f"Total: ${net_total_profit_loss}\n")
+    txt_file.write(f"Average Change: ${ave_change_total}\n")
+    txt_file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n")
+
+
+
+
+    
+        
+
+
+         
+                                
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+
+
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# python-challenge
+I created a repository on Git Hub called python-challenge. Cloned it and pulled it to my computer. Using terminal created needed folders and files for PyBank and PyPoll.  
+My python-challenge repository contans the main python script file, csv file, and the outcome file for each folder. Here I am providing the outcome results for PyBank and PyPoll scripts. 
+
+Financial Analysis
+--------------------------------
+Total Months: 86
+Total: $22564198
+Average Change: $-8311.11
+Greatest Increase in Profits: Feb-17 ($1862002)
+Greatest Decrease in Profits: Feb-17 ($-1825558)
+
+
+
+
+
+Election Results
+------------------------------
+Total Votes: 369711
+------------------------------
+Charles Casper Stockham: 23.049% (85213)
+Diana DeGette: 73.812% (272892)
+Raymon Anthony Doane: 3.139% (11606)
+------------------------------
+Winner: Diana DeGette
